9e57421f88b65abb334365b88913be212b639553
Melindadiaz07 <melindadiaz@me.com>
2020-08-27 11:04:58 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
+   
 
+   self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
+   
+  
   end
+
+    
+   
+
+  
 end
+
